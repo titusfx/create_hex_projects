ebf403b239db5fb3ce4eb3cedf58c7d12875d912
titus fx <titusfx@gmail.com>
2023-12-19 15:16:13 +0100

    Working

--- create_hex_project.py
+++ create_hex_project.py
@@ -14,34 +14,30 @@ def init_project(project_name):
     Initializes the project structure for a given project name.
     """
     base_structure = {
-        f'{project_name}/': ['application/', 'domain/', 'infrastructure/', 'interfaces/'],
-        f'{project_name}/application/': ['__init__.py'],
-        f'{project_name}/domain/': ['__init__.py', 'models/', 'exceptions/', 'repositories/', 'services/'],
-        f'{project_name}/infrastructure/': ['__init__.py', 'adapters/', 'cache/', 'db/', 'repositories/', 'services/'],
+        f'{project_name}/': ['application', 'domain', 'infrastructure', 'interfaces'],
+        f'{project_name}/domain/': ['models', 'exceptions', 'repositories', 'services'],
+        f'{project_name}/infrastructure/': ['adapters', 'cache', 'db', 'repositories', 'services'],
         f'{project_name}/interfaces/': ['api_rest/'],
-        f'{project_name}/interfaces/api_rest/': ['__init__.py', 'models/', 'routes/'],
-        'tests/': ['__init__.py', f'{project_name}/'],
-        f'tests/{project_name}/': ['domain/', 'interfaces/']
+        f'{project_name}/interfaces/api_rest/': ['models', 'routes'],
+        'tests/': [f'{project_name}/'],
+        f'tests/{project_name}/': ['domain', 'interfaces']
     }
     
-   # Create directories
+    # Create directories
     for path, subdirs in base_structure.items():
-        os.makedirs(path, exist_ok=True)  # Ensure the root directory exists
         for subdir in subdirs:
             dir_path = os.path.join(path, subdir)
             os.makedirs(dir_path, exist_ok=True)  # Create subdirectories
-            # Create an __init__.py file if the subdir is not a file path
-            if not subdir.endswith('.py'):
-                init_path = os.path.join(dir_path, '__init__.py')
-                open(init_path, 'a').close()
-            
+
     # Create __init__.py files for all directories
-    for root, dirs, files in os.walk(project_name):
-        for dir in dirs:
-            init_path = os.path.join(root, dir, '__init__.py')
-            open(init_path, 'a').close()  # 'a' to ensure we don't overwrite existing files
+    for path in base_structure.keys():
+        for root, dirs, _ in os.walk(path):
+            for dir in dirs:
+                init_path = os.path.join(root, dir, '__init__.py')
+                open(init_path, 'a').close()  # 'a' to ensure we don't overwrite existing files
 
     click.echo(f'Project {project_name} has been initialized successfully.')
 
 if __name__ == '__main__':
     cli()
+
